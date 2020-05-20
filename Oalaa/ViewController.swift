fa82aec61cdb3de251926fb59675f2a0addf68c3
Rizal Hidayat <realrizalhidayat@gmail.com>
2020-05-20 10:24:05 +0700

    Initial Commit

---
+++ Oalaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oalaa
+//
+//  Created by Rizal Hidayat on 20/05/20.
+//  Copyright © 2020 M2-911. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
