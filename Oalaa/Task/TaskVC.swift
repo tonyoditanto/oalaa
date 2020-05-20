5c6ff64e9fa5591cf2165ea01996172088a70061
Sw <swrhythm95@gmail.com>
2020-05-20 11:31:02 +0700

    ViewController Mapping

---
+++ Oalaa/Task/TaskVC.swift
@@ -0,0 +1,30 @@
+//
+//  TaskVC.swift
+//  Oalaa
+//
+//  Created by Steven Wijaya on 20/05/20.
+//  Copyright © 2020 M2-911. All rights reserved.
+//
+
+import UIKit
+
+class TaskVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
