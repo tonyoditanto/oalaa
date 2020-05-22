45df59a8cc4944bd2028d2332dc0974daeae4013
Sw <swrhythm95@gmail.com>
2020-05-22 08:52:23 +0700

    create entity for core data

---
+++ Oalaa/Classes/Categories.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Oalaa
+//
+//  Created by Steven Wijaya on 22/05/20.
+//  Copyright © 2020 M2-911. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Category: NSObject {
+	var objectID: NSManagedObjectID!
+	var categoryName: String!
+	var installed: Bool!
+}
