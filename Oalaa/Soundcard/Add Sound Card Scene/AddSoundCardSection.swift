c884178a2a7608b405cdf52ff81b960d1b2d4eb2
Tony Varian Yoditanto <tonyoditanto@gmail.com>
2020-05-24 23:27:26 +0700

    Add : AddSoundCardSection.swift

---
+++ Oalaa/Soundcard/Add Sound Card Scene/AddSoundCardSection.swift
@@ -0,0 +1,15 @@
+//
+//  AddSoundCardSection.swift
+//  Oalaa
+//
+//  Created by Tony Varian Yoditanto on 24/05/20.
+//  Copyright © 2020 M2-911. All rights reserved.
+//
+
+import Foundation
+struct AddSoundCardSection {
+    static let SECTION_HEADER = 0
+    static let SECTION_OBJECT_RECOGNITION = 1
+    static let SECTION_PRE = 2
+    static let SECTION_POST = 3
+}
