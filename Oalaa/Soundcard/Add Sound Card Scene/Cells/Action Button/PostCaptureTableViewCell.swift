2ca71b0cb6d88f422c9077de8b85a81341cae9ac
Tony Varian Yoditanto <tonyoditanto@gmail.com>
2020-05-25 13:42:41 +0700

    Done : Access Camera (Integrate Camera using AVFoundation)
    On Progress :
    Blocker - Image Processing (Sizing Preview into square)
    Capture Image
    Object Recognisition
    Text to Speech
    Save Capture Image and object name

--- Oalaa/Soundcard/Add Sound Card Scene/Cells/Action Button/PostCaptureTableViewCell.swift
+++ Oalaa/Soundcard/Add Sound Card Scene/Cells/Action Button/PostCaptureTableViewCell.swift
@@ -13,7 +13,6 @@ class PostCaptureTableViewCell: UITableViewCell {
     @IBOutlet weak var speakButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Oalaa/Soundcard/Add Sound Card Scene/Cells/Object Recognition/PreCapture/ObjectRecognitionTableViewCell.swift
+++ Oalaa/Soundcard/Add Sound Card Scene/Cells/Object Recognition/PreCapture/ObjectRecognitionTableViewCell.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ObjectRecognitionTableViewCell: UITableViewCell {
     static let cellID = "ObjectRecognitionTableViewCell"
     @IBOutlet weak var captureButton: UIButton!
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var objectNameLabel: UILabel!
+    
+    var captureSession : AVCaptureSession?
+    var videoPreviewLayer : AVCaptureVideoPreviewLayer?
+    var frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
+    var backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         configureComponentDesign()
+        registerCamera()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -26,6 +35,23 @@ class ObjectRecognitionTableViewCell: UITableViewCell {
     
     func configureComponentDesign(){
         captureButton.layer.cornerRadius = 10.0
+    }
+    
+    func registerCamera(){
+        let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
+            videoPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+            videoPreviewLayer?.frame = cameraView.layer.bounds
+            cameraView.layer.addSublayer(videoPreviewLayer!)
+            captureSession?.startRunning()
+        } catch  {
+            print("error")
+        }
     }
     
     @IBAction func didTapCaptureButton(_ sender: Any) {
