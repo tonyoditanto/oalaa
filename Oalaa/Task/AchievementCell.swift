aaeeef13dded158a066ff4ae1d963e4dc5f5e88d
Rizal Hidayat <realrizalhidayat@gmail.com>
2020-05-28 09:35:25 +0700

    add dailymission tableview

---
+++ Oalaa/Task/AchievementCell.swift
@@ -0,0 +1,13 @@
+//
+//  AchievementCell.swift
+//  Oalaa
+//
+//  Created by Rizal Hidayat on 28/05/20.
+//  Copyright © 2020 M2-911. All rights reserved.
+//
+
+import UIKit
+
+class AchievementCell: UICollectionViewCell {
+    
+}
