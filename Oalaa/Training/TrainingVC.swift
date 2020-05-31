4ebcabfa9e44d8a90a63a20f37dc10eeb6b01f5f
Taufiq Ramadhany <taufiq_ramadhany@yahoo.com>
2020-06-01 03:08:40 +0700

    nambahi flip animasi

--- Oalaa/Training/TrainingVC.swift
+++ Oalaa/Training/TrainingVC.swift
@@ -20,7 +20,7 @@ class TrainingVC: UIViewController {
     var dataManager: DataManager = DataManager()
     lazy var activeCategory: NSManagedObject = dataManager.getCategory(coreVocab: false, installed: true, index: 0)
     var answerText: String = ""
-    var defaultImage = UIImage (named: "dionTraining")
+    var defaultImage = UIImage (named: "AppIcon")
     var defaultCard = false
     
     override func viewDidLoad() {
@@ -45,29 +45,32 @@ class TrainingVC: UIViewController {
     @IBAction func reloadTrainingImage(_ sender: Any) {
         let fetchRandomSoundcard: NSManagedObject = dataManager.getRandomInstalledSoundcard()
         
-        // displaying name of soundcard
-        nameOfTrainingImage.text = fetchRandomSoundcard.value(forKey: "soundcardName") as? String
-        
-        let utterance = AVSpeechUtterance(string: fetchRandomSoundcard.value(forKey: "soundcardName") as! String)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en")
-        utterance.rate = 0.5
-        let synthesizer = AVSpeechSynthesizer()
-        synthesizer.speak(utterance)
-        answerText = nameOfTrainingImage.text!
-        
-        TaskManager.addAction(action: .speak)
-        playbuttonText.setTitle("Next Card", for: .normal)
-        
         // flip animation added
         if defaultCard {
             defaultCard = false
             trainingDefaultImage.image = defaultImage
             UIView.transition(with: trainingDefaultImage, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
-        } else {
+            
+            playbuttonText.setTitle("Next Card", for: .normal)
+            nameOfTrainingImage.text = ""
+        }
+        else {
             defaultCard = true
             trainingDefaultImage.image = dataManager.getSoundcardImageFor(soundcard: fetchRandomSoundcard)
-            defaultImage = trainingDefaultImage.image
+            //defaultImage = trainingDefaultImage.image
             UIView.transition(with: trainingDefaultImage, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+            // displaying name of soundcard
+            nameOfTrainingImage.text = fetchRandomSoundcard.value(forKey: "soundcardName") as? String
+            
+            let utterance = AVSpeechUtterance(string: fetchRandomSoundcard.value(forKey: "soundcardName") as! String)
+            utterance.voice = AVSpeechSynthesisVoice(language: "en")
+            utterance.rate = 0.5
+            let synthesizer = AVSpeechSynthesizer()
+            synthesizer.speak(utterance)
+            answerText = nameOfTrainingImage.text!
+            
+            TaskManager.addAction(action: .speak)
+            playbuttonText.setTitle("Next Card", for: .normal)
         }
     }
     
