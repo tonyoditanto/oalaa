413d26feb2c43508260df06ad677d2115d1ef832
Taufiq Ramadhany <taufiq_ramadhany@yahoo.com>
2020-06-01 02:29:04 +0700

    nambahin flip animasi

--- Oalaa/Training/TrainingVC.swift
+++ Oalaa/Training/TrainingVC.swift
@@ -15,10 +15,14 @@ class TrainingVC: UIViewController {
 
     @IBOutlet weak var trainingDefaultImage: UIImageView!
     @IBOutlet weak var nameOfTrainingImage: UILabel!
+    @IBOutlet weak var playbuttonText: UIButton!
     
     var dataManager: DataManager = DataManager()
     lazy var activeCategory: NSManagedObject = dataManager.getCategory(coreVocab: false, installed: true, index: 0)
     var answerText: String = ""
+    var defaultImage = UIImage (named: "dionTraining")
+    var defaultCard = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,39 +33,6 @@ class TrainingVC: UIViewController {
     // text to speech part
     
     @IBAction func playTrainingButton(_ sender: UIButton) {
-        let utterance = AVSpeechUtterance(string: textToSpeech)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en")
-        utterance.rate = 0.5
-
-        let synthesizer = AVSpeechSynthesizer()
-        synthesizer.speak(utterance)
-        
-    }
-        
-    // text variable
-    var textToSpeech = "Say the word"
-    
-    
-    @IBAction func reloadTrainingImage(_ sender: Any) {
-        let fetchRandomSoundcard: NSManagedObject = dataManager.getRandomInstalledSoundcard()
-        
-        // resize image to fit 350 * 350
-        let originalImageSize = dataManager.getSoundcardImageFor(soundcard: fetchRandomSoundcard)
-        let newImageSize = originalImageSize.scaleImageToFitSize(size: CGSize(width: 350.0, height: 350.0))
-        trainingDefaultImage.image = newImageSize.scaleImageToFitSize(size: CGSize(width: 350.0, height: 350.0))
-        //trainingDefaultImage.image = dataManager.getSoundcardImageFor(soundcard: fetchRandomSoundcard)
-        nameOfTrainingImage.text = fetchRandomSoundcard.value(forKey: "soundcardName") as? String
-        let utterance = AVSpeechUtterance(string: fetchRandomSoundcard.value(forKey: "soundcardName") as! String)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en")
-        utterance.rate = 0.5
-
-        let synthesizer = AVSpeechSynthesizer()
-        synthesizer.speak(utterance)
-        answerText = nameOfTrainingImage.text!
-        
-    }
-    
-    @IBAction func playTheAnswerButton(_ sender: Any) {
         let utterance = AVSpeechUtterance(string: answerText)
         utterance.voice = AVSpeechSynthesisVoice(language: "en")
         utterance.rate = 0.5
@@ -69,29 +40,39 @@ class TrainingVC: UIViewController {
         let synthesizer = AVSpeechSynthesizer()
         synthesizer.speak(utterance)
         
+    }
+        
+    
+    @IBAction func reloadTrainingImage(_ sender: Any) {
+        let fetchRandomSoundcard: NSManagedObject = dataManager.getRandomInstalledSoundcard()
+        
+        // displaying name of soundcard
+        nameOfTrainingImage.text = fetchRandomSoundcard.value(forKey: "soundcardName") as? String
+        
+        let utterance = AVSpeechUtterance(string: fetchRandomSoundcard.value(forKey: "soundcardName") as! String)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en")
+        utterance.rate = 0.5
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+        answerText = nameOfTrainingImage.text!
+        
         TaskManager.addAction(action: .speak)
-
-    }
-    
-    
-}
-
-extension UIImage {
-
-    func scaledImage(withSize size: CGSize) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-        defer { UIGraphicsEndImageContext() }
-        draw(in: CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height))
-        return UIGraphicsGetImageFromCurrentImageContext()!
-    }
-
-    func scaleImageToFitSize(size: CGSize) -> UIImage {
-        let aspect = self.size.width / self.size.height
-        if size.width / aspect <= size.height {
-            return scaledImage(withSize: CGSize(width: size.width, height: size.width / aspect))
+        playbuttonText.setTitle("Next Card", for: .normal)
+        
+        // flip animation
+        if defaultCard {
+            defaultCard = false
+            trainingDefaultImage.image = defaultImage
+            UIView.transition(with: trainingDefaultImage, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
         } else {
-            return scaledImage(withSize: CGSize(width: size.height * aspect, height: size.height))
+            defaultCard = true
+            trainingDefaultImage.image = dataManager.getSoundcardImageFor(soundcard: fetchRandomSoundcard)
+            defaultImage = trainingDefaultImage.image
+            UIView.transition(with: trainingDefaultImage, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
         }
+        
+        
     }
-
+    
+    
 }
